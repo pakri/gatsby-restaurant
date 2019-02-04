f09ce81fab6cc738af2b7c76f8c76b1b7f138215
pakri <pakrischke@gmail.com>
2019-02-04 18:44:00 +0100

    git push origin master

---
+++ src/components/globals/navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import NavbarHeader from "./NavbarHeader";
+import NavbarLinks from "./NavbarLinks";
+import NavbarIcons from "./NavbarIcons";
+import styled from "styled-components";
+
+export default class Navbar extends Component {
+  state = {
+    navbarOpen: false
+  };
+  handleNavbar = () => {
+    this.setState(() => {
+      return { navbarOpen: !this.state.navbarOpen };
+    });
+  };
+  render() {
+    return (
+      <nav>
+        <NavbarHeader handleNavbar={this.handleNavbar} />
+        <NavbarLinks navbarOpen={this.state.navbarOpen} />
+        <NavbarIcons />
+      </nav>
+    );
+  }
+}
+
+const NavWrapper = styled.nav`
+  @media (min-width: 768px) {
+    display: flex;
+    align-items: center;
+  }
+`
---
+++ src/components/globals/navbar/NavbarIcons.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class NavbarIcons extends Component {
+  render() {
+    return (
+      <div>content from navbar icons</div>
+    )
+  }
+}
